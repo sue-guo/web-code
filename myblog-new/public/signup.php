82be6b04079efe114a1fc4859a44cefb9f84b4a1
Hongxiu Guo <hxiuguo@gmail.com>
2024-03-28 00:40:33 -0400

    check用户是否已经存在

--- myblog-new/public/signup.php
+++ myblog-new/public/signup.php
@@ -5,15 +5,24 @@
 
     require_once('../private/db_connect.php');
     include_once('../private/db_functions.php');
+    $errors = array('username' => '', 'password' => '');
 
     if(!empty($_POST['username']) && !empty($_POST['password'])){
         $username = $_POST['username'];
         $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-        $sql = "insert into users(username,passwd) values('$username','$password')";
-        if(mysqli_query($conn,$sql)){
-            header('Location: login.php');
+        
+        $sql = "select * from users where username = '$username'";
+        $result = mysqli_query($conn, $sql);
+        
+        if(mysqli_num_rows($result) > 0){
+            $errors['username'] = 'user already exists';
         }else{
-            echo 'sign up query error: ' . mysqli_error($conn);
+            $sql = "insert into users(username,passwd) values('$username','$password')";
+            if(mysqli_query($conn,$sql)){
+                header('Location: login.php');
+            }else{
+                echo 'sign up query error: ' . mysqli_error($conn);
+            }
         }
     }
     close_connection($conn);
@@ -28,22 +37,20 @@
             </p>
             <div class="textfield">
                 <label for="username">user name:</label>
-                <input type="text" name="username" id="username">
-                <span class="errorMsg" id="username_span"></span>
+                <input type="text" name="username" id="username" value="<?php if(isset($_POST['username'])) echo $_POST['username']?>">
+                <span class="errorMsg" id="username_span"><?php echo $errors['username']?></span>
             </div>
             <div class="textfield">
                 <label for="password">password:</label>
-                <input type="password" name="password" id="password">
+                <input type="password" name="password" id="password" value="<?php if(isset($_POST['password'])) echo $_POST['password']?>">
                 <span class="errorMsg" id="password_span"></span>
             </div>
             <div class="textfield">
                 <label for="confirm_password">confirm password:</label>
-                <input type="password" id="confirm_password">
+                <input type="password" name="comfirmPassword" id="confirm_password" value="<?php if(isset($_POST['comfirmPassword'])) echo $_POST['comfirmPassword']?>">
                 <span class="errorMsg" id="confirm_password_span"></span>
             </div>
             <div class="buttonfield">
-                <!-- <button type="submit" class="round_border login_button">Sign Up</button>
-                <button type="reset" class="round_border login_button" id="reset">Reset</button> -->
                 <input type="submit" name="submit" id="submit" value="Sign Up" class="round_border login_button">
                 <input type="reset" name="reset" id="reset" value="Reset" class="round_border login_button">
             </div>
